4d21cd2e465d7c90fb0cec29d0e3c71a5ae239bf
Garrett Tjarks <chicarksey@gmail.com>
2014-03-29 18:26:46 -0400

    another complex test
    since this has a differenct constructor, then i may as well test to make
    sure it works

--- src/test/java/spinnytea/programmagic/maze/TestAlgorithms.java
+++ src/test/java/spinnytea/programmagic/maze/TestAlgorithms.java
@@ -24,7 +24,6 @@ public class TestAlgorithms
 	@Test
 	public void depthFirst_Simple()
 	{
-		// small board for initial test case
 		Cell2D[][] cells = new DepthFirstMaze(2, 2).generateMaze(1L);
 		Cell2D[][] expected = createCells(2, 2);
 		setRow(expected, 0, EAST, SOUTH);
@@ -44,7 +43,6 @@ public class TestAlgorithms
 	@Test
 	public void depthFirst_Complex()
 	{
-		// small board for initial test case
 		Cell2D[][] cells = new DepthFirstMaze(6, 6).generateMaze(1L);
 
 		Cell2D[][] expected = createCells(6, 6);
@@ -54,6 +52,23 @@ public class TestAlgorithms
 		setRow(expected, 3, EAST, SOUTH, EAST, NORTH, SOUTH, SOUTH);
 		setRow(expected, 4, NORTH, EAST, SOUTH, EAST, SOUTH, SOUTH);
 		setRow(expected, 5, NORTH, WEST, EAST, NORTH, EAST, null);
+
+		assertArrayEquals(cells, expected);
+	}
+
+	@Test
+	public void depthFirst_Complex2()
+	{
+
+		Cell2D[][] cells = new DepthFirstMaze(6, 6, 3, 3).generateMaze(2L);
+
+		Cell2D[][] expected = createCells(6, 6);
+		setRow(expected, 0, EAST, SOUTH, EAST, EAST, EAST, SOUTH);
+		setRow(expected, 1, NORTH, EAST, NORTH, SOUTH, WEST, WEST);
+		setRow(expected, 2, NORTH, WEST, SOUTH, WEST, SOUTH, WEST);
+		setRow(expected, 3, NORTH, SOUTH, WEST, null, WEST, NORTH);
+		setRow(expected, 4, NORTH, SOUTH, NORTH, WEST, EAST, NORTH);
+		setRow(expected, 5, NORTH, EAST, EAST, EAST, NORTH, WEST);
 
 		assertArrayEquals(cells, expected);
 	}
