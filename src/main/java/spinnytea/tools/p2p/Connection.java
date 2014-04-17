82b4174b75dc50b96bcc78a5607ac1a7e777f32f
Garrett Tjarks <chicarksey@gmail.com>
2014-04-17 14:34:33 -0400

    basic server implementation
    with an abstract class that can act as either a server consumer or a
    client

---
+++ src/main/java/spinnytea/tools/p2p/Connection.java
@@ -0,0 +1,192 @@
+package spinnytea.tools.p2p;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This class is designed to send and receive java objects. Implementations are basically state machines that will handle objects as they become available. This
+ * class enforces a protocol; it will send a protocol and wait to receive a protocol from the connection.
+ * <p>
+ * Implementations must have a NoArgsConstructor
+ * <p>
+ * Pro Tip: It is probably a good design to send a whole message/action as a single object.
+ */
+@Slf4j
+@NoArgsConstructor
+public abstract class Connection
+implements Runnable
+{
+	/** the socket to read from for this client */
+	@Getter
+	private Socket mySocket;
+	/** the server that this connection belongs to ~ if we remove this, then the server will need to periodically check to see if it's still alive */
+	private Server<?> myServer;
+
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
+
+	/** has the socket started to close itself? this just ensures we don't try to close it multiple times */
+	private boolean running;
+
+	/**
+	 * @param server this can be null; if it is, then it will not be managed by anything; it will be a basic client connection
+	 */
+	protected final void start(Socket socket, Server<?> server)
+	throws IOException
+	{
+		mySocket = socket;
+		mySocket.setKeepAlive(true);
+		ois = new ObjectInputStream(mySocket.getInputStream());
+		oos = new ObjectOutputStream(mySocket.getOutputStream());
+		running = true;
+
+		if(server != null)
+		{
+			myServer = server;
+			myServer.addConnection(this);
+		}
+
+		// start a thread to listen to the socket
+		new Thread(this).start();
+	}
+
+	@Override
+	public final void run()
+	{
+		try
+		{
+			// first, send the protocol that we will be using
+			sendObject(getProtocol());
+
+			try
+			{
+				// now, wait for them to send us the protocol they will be using
+				if(!acceptProtocol((String) ois.readObject()))
+				{
+					log.warn("The protocol from this connection is not acceptable.");
+					close();
+				}
+			}
+			catch(ClassCastException | ClassNotFoundException | IOException e)
+			{
+				if(myServer == null || myServer.isRunning())
+				{
+					// should this be an error?
+					log.warn("Invalid connection from (" + mySocket.getInetAddress().toString() + "); I wonder what they wanted.", e);
+					close();
+				}
+			}
+
+			// now that everything is setup, continually read objects from the connection
+			// when the connection has broken, or closed, then we will gracefully exit
+			while(running && (myServer == null || myServer.isRunning()))
+			{
+				try
+				{
+					// read an object
+					Object obj = ois.readObject();
+					// pass it to the class to handle
+					nextObject(obj);
+				}
+				catch(ClassNotFoundException | IOException e)
+				{
+					// the socket broke, so, lets shut it down
+					if(myServer == null || myServer.isRunning())
+					{
+						log.warn("A socket (" + mySocket.getPort() + ") has terminated", e);
+						close();
+					}
+				}
+			}
+		}
+		finally
+		{
+			// just in case
+			close();
+		}
+	}
+
+	protected void sendObject(Object obj)
+	{
+		try
+		{
+			oos.writeObject(obj);
+			oos.flush();
+		}
+		catch(IOException e)
+		{
+			// the socket broke, so, lets shut it down
+			if(myServer == null || myServer.isRunning())
+			{
+				log.warn("A socket (" + mySocket.getPort() + ") has terminated", e);
+				close();
+			}
+		}
+	}
+
+	/** when we are finished with the connect, then close it ~ this will call cleanup */
+	protected void close()
+	{
+		if(running)
+		{
+			running = false;
+			if(myServer != null)
+				myServer.removeConnection(this);
+
+			// clean up
+			cleanup();
+
+			try
+			{
+				oos.close();
+			}
+			catch(IOException e)
+			{
+				log.error("An I/O error occured when closing the socket OutputStream", e);
+			}
+			try
+			{
+				ois.close();
+			}
+			catch(IOException e)
+			{
+				log.error("An I/O error occured when closing the socket InputStream", e);
+			}
+			try
+			{
+				mySocket.close();
+			}
+			catch(IOException e)
+			{
+				log.warn("An I/O error occured when closing this socket", e);
+			}
+		}
+	}
+
+	/**
+	 * This is the protocol that will be sent as soon as we start the connection.
+	 * <p>
+	 * It is recommended that the protocol have a name and number; what is the protocol for, and what iteration is it on. (e.g. peer_v1 or server_v1)
+	 */
+	protected abstract String getProtocol();
+
+	/**
+	 * the first thing the connection should receive is a protocol. the protocol acts as a contract between producer and consumer. It let's the consumer know
+	 * what kind of data to expect from the producer.
+	 * 
+	 * @return true if the connection will be accepted; false if the connection should be closed
+	 */
+	protected abstract boolean acceptProtocol(String protocol);
+
+	/** the connection is sending another object; here it is */
+	protected abstract void nextObject(Object obj);
+
+	/** the connection is going to close NOW, so take care of your last rights; this may be called at any time */
+	protected abstract void cleanup();
+}
---
+++ src/main/java/spinnytea/tools/p2p/Server.java
@@ -0,0 +1,130 @@
+package spinnytea.tools.p2p;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import lombok.Getter;
+import lombok.Synchronized;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * This class is a server in that it needs to listen on a port for another connection. This is used for both a "client-server" model as well as a "peer-to-peer"
+ * model.
+ */
+@Slf4j
+public class Server<T extends Connection>
+implements Runnable
+{
+	/** the class that will do the consuming */
+	private Class<T> consumerClazz;
+
+	/** the socket to listen on */
+	private ServerSocket serverSocket;
+
+	/** when we are supposed to stop, then this will be set to false; this will signal to the connections to disconnect */
+	@Getter
+	// XXX can we remove the getter
+	private boolean running;
+
+	/** a list to store the current connects; this way they can be shut down */
+	private Collection<Connection> connections;
+
+	public Server(int port, Class<T> consumerClazz)
+	throws IOException
+	{
+		this.consumerClazz = consumerClazz;
+		serverSocket = new ServerSocket(port);
+		connections = new LinkedList<Connection>();
+	}
+
+	@Synchronized("connections")
+	public void addConnection(Connection conn)
+	{
+		connections.add(conn);
+		log.warn("New connection!" + //
+		"\n\tFrom: " + conn.getMySocket().getInetAddress().toString() + ":" + conn.getMySocket().getPort() + //
+		"\n\tTo:   " + conn.getMySocket().getLocalSocketAddress().toString());
+		log.debug("There are now " + connections.size() + " connections");
+	}
+
+	@Synchronized("connections")
+	public void removeConnection(Connection conn)
+	{
+		connections.remove(conn);
+		log.info("Closing connection!" + //
+		"\n\tFrom: " + conn.getMySocket().getInetAddress().toString() + ":" + conn.getMySocket().getPort() + //
+		"\n\tTo:   " + conn.getMySocket().getLocalSocketAddress().toString());
+		log.debug("There are now " + connections.size() + " connections");
+	}
+
+	@Override
+	public void run()
+	{
+		log.info("Starting server");
+
+		try
+		{
+			while(running)
+			{
+				log.debug("Waiting for a connection");
+				try
+				{
+					Socket socket = serverSocket.accept();
+					Connection conn = null;
+
+					try
+					{
+						conn = consumerClazz.newInstance();
+						conn.start(socket, this);
+					}
+					catch(Exception e)
+					{
+						log.error("An error occured while creating a ServerConsumer", e);
+						if(conn != null)
+							conn.close();
+					}
+				}
+				catch(IOException e)
+				{
+					if(running)
+						log.error("An I/O error occurs when waiting for a connection", e);
+					// don't stop the server from this
+				}
+			} // end while(running)
+		}
+		finally
+		{
+			// just in case
+			if(running)
+				close();
+		}
+
+		log.info("End of run method reached for the Server");
+	}
+
+	public void close()
+	{
+		log.info("Shutting down the server.");
+
+		// tell everything that it's time to stop
+		running = false;
+
+		// shutdown the connections manually
+		while(!connections.isEmpty())
+			connections.iterator().next().close();
+
+		// now that everything is closed, shut down the server
+		// this will cause the .accept() method to throw an exception and stop
+		try
+		{
+			serverSocket.close();
+		}
+		catch(Exception e)
+		{
+			log.warn("Error while shutting down server.", e);
+		}
+	}
+}
