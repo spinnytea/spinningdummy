8bfc222009bb5d6e07d64914a898a39dbce4b353
Garrett Tjarks <chicarksey@gmail.com>
2014-04-25 23:40:07 -0400

    major refactor part 2 of 3?
    ..

--- src/main/java/spinnytea/clone/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package is for programs that I have found elsewhere, and I decided that I wanted to try my hand at making.
- * <p/>
- * These will always be created by me, for me, but the original ideas are specifically plagiarized.
- * It's sort of like a writer or an artist copying their favorite author. It's just a learning exercise.
- */
-package spinnytea.clone;
--- src/main/java/spinnytea/tools/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * These packages and classes can't stand alone.
- * They are just useful tools or utilities that I have created or found, and have archived for use and reuse.
- */
-package spinnytea.tools;
